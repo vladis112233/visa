e2fcc28c1bd61dca73c5a569f9935583c5ecd90e
vladis112233 <124692818+vladis112233@users.noreply.github.com>
2025-02-03 16:19:21 +0200

    19

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ toggleButton.addEventListener('click', () => {
         toggleButton.textContent = 'Show Less';
     }
 });
+
+document.getElementById("langMenu").addEventListener("click", function () {
+    var dropdown = document.getElementById("dropdownMenu");
+    if (dropdown.style.display === "block") {
+        dropdown.style.display = "none";
+    } else {
+        dropdown.style.display = "block";
+    }
+});
+
+// Закрытие меню при клике вне него
+document.addEventListener("click", function (event) {
+    var menu = document.getElementById("langMenu");
+    var dropdown = document.getElementById("dropdownMenu");
+
+    if (!menu.contains(event.target)) {
+        dropdown.style.display = "none";
+    }
+});
